660b09314ddd8f075c841ef6805f28c27ff33fcf
Kyle Barnett <kbarnett23@gmail.com>
2019-11-09 10:50:56 -0700

    POST project working

--- data/projectRouter.js
+++ data/projectRouter.js
@@ -12,4 +12,19 @@ router.get('/', (req, res) => {
     })
 })
 
+router.post('/', (req, res) => {
+  const bodyInfo = req.body;
+  if (!bodyInfo.name || !bodyInfo.description) {
+    res.status(404).json({ message: 'Must have a name and description.' })
+  } else {
+    db.insert(bodyInfo)
+      .then(info => {
+        res.status(201).json(info)
+      })
+      .catch(err => {
+        res.status(500).json({ message: 'There was an issue posting to the DB.' })
+      })
+  }
+})
+
 module.exports = router;
